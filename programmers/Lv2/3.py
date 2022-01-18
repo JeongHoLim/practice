7048943a45e2a367f7ffd63d034f13ae2908ed7c
JeongHoLim <9997ijh@gmail.com>
2022-01-19 01:26:16 +0900

    124 나라의 숫자

---
+++ programmers/Lv2/3.py
@@ -0,0 +1,15 @@
+
+# https://programmers.co.kr/learn/courses/30/lessons/12899?language=python3
+
+def solution(n):
+    answer = ''
+    my_dict = ['4','1','2']
+    
+    r = n
+    while r>0:
+        r,q = divmod(r,3)
+        answer += my_dict[q]
+        if q== 0:
+            r -= 1
+    
+    return answer[::-1]
