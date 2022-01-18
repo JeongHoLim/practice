f6d5364d3e17b3f3290bd4463c7e401c27b75dcf
JeongHoLim <9997ijh@gmail.com>
2022-01-19 01:26:00 +0900

    멀쩡한 사각형

---
+++ programmers/Lv2/2.py
@@ -0,0 +1,31 @@
+
+# https://programmers.co.kr/learn/courses/30/lessons/62048?language=python3
+
+from math import ceil,gcd,floor
+def solution(w,h):
+    
+    answer = w*h
+    useless = 0
+    
+    nh = max(h,w)
+    nw = min(h,w)
+    
+    a = nh / nw
+    
+    g = gcd(nh,nw)
+    dw,dh = nw // g, nh//g
+    
+    
+    for i in range(dw):
+        f1 = a*i+dh
+        f2 = a*(i+1) + dh
+        useless += (ceil(f2)-floor(f1))
+        
+        
+    if nw != dw:
+        useless *= g
+    print(f"no:{useless}, a:{a}, g: {g},nw:{nw},dw:{dw}")
+    answer -= useless
+    
+    
+    return answer
